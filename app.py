57320012c34374b15c136774f8e22eeb644855db
Genevieve <genevieve.okon@gmail.com>
2021-11-03 02:18:08 -0400

    nfl rush code update; changes to app.py and new base html file to be inherited by index.html. New libraries added as well

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from flask import Flask, json, render_template,request,jsonify
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='./static')
 
 # read file
 with open('./rushing.json', 'r') as myfile:
     data = myfile.read()
 
-
-
+'''
+endpoint for returning data from json
+'''
+@app.route('/somedata')
+def datas(): 
+    #parsing json string with json.loads method
+    if data:
+        json_data = json.loads(data) #json_data is a list with dictionaries (rushing.json)
+    else: #if json is empty
+        return {'data': []}
+    
+    return {'data': json_data}
+        
+'''
+endpoint for rendering index page with table
+'''
 @app.route("/")
 def index():
-    #parsing json string with json.loads method
-    #result creates a Python dictionary 
-    json_data = json.loads(data)
-    #json_data is a list with dictionaries
-    return render_template('index.html', title="page", jsonfile=json_data)
-
+    return render_template('index.html', title="NFL Rush")
 
 if __name__ == "__main__":
     app.run(debug=True)
